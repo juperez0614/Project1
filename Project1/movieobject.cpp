ce849da7f76553f1f2aba63b7d5a65c34f8bdcba
asmusj <asmusj@users.noreply.github.com>
2014-06-02 16:58:25 -0700

    updated comments

--- Project1/movieobject.cpp
+++ Project1/movieobject.cpp
@@ -12,10 +12,16 @@
 //
 // Assumptions:
 //   -- quantity defaults to a fixed size of 10 if quantity is not specified
+//   -- format is set to D because all have formats of dvd
 //   -- all other attributes default to empty chars, strings, or to 0
 //---------------------------------------------------------------------------
 #include "movieobject.h"
 
+//---------------------------------------------------------------------------
+//constructor
+//sets attributes 
+//format = D because DVD all formats are dvd
+//quantity = 10 default fixed size
 MovieObject::MovieObject():Object(){
 	format = 'D';
 	title = "";
@@ -31,6 +37,9 @@ MovieObject::MovieObject(string movieTitle, int yr, string dir):Object(){
 	quantity = 10;
 }
 
+//---------------------------------------------------------------------------
+//destructor
+//sets attrubutes to empty or zero
 MovieObject::~MovieObject(){
 	title = "";
 	year = 0;
@@ -38,10 +47,16 @@ MovieObject::~MovieObject(){
 	quantity = 0;
 }
 
+//---------------------------------------------------------------------------
+//getQuantity
+//returns the number of movie objects
 int MovieObject::getQuantity() const{
 	return quantity;
 }
 
+//---------------------------------------------------------------------------
+//setQuantity
+//sets quantity to new value
 bool MovieObject::setQuantity(int change){
 	quantity = quantity + change; 
 	return true;
