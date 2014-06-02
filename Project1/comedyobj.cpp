15699e78319eae2dc06ed4f23f80ea4b8a7eb31e
asmusj <asmusj@users.noreply.github.com>
2014-06-02 16:00:36 -0700

    added comments

--- Project1/comedyobj.cpp
+++ Project1/comedyobj.cpp
@@ -1,3 +1,18 @@
+//---------------------------------------------------------------------------
+// ComedyObj.H
+// Derived class from movieobject, contains comedy types
+// Authors: Julio Perez, Jeffrey Asmus
+//---------------------------------------------------------------------------
+// ComedyObj class:  
+//		includes  features:
+//   -- creates comedy object
+//   -- sets data in object
+//   -- allows for partial data to be set
+//   -- allows for comparsion of 2 classic objects
+//   -- displays classic object contents
+//   -- displays actions performed on classic objects
+//
+//---------------------------------------------------------------------------
 #include "comedyobj.h"
 
 ComedyObj::ComedyObj() :MovieObject(){}
