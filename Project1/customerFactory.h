277e42370a92f8404a068d0f33499a89b7b04756
asmusj <asmusj@users.noreply.github.com>
2014-06-02 17:21:46 -0700

    updated comments

--- Project1/customerFactory.h
+++ Project1/customerFactory.h
@@ -1,3 +1,17 @@
+//---------------------------------------------------------------------------
+// customerFactory.h
+// derived class for factorys, stores movie objects
+// Authors: Julio Perez, Jeffrey Asmus
+//---------------------------------------------------------------------------
+// customerfactor class:  
+//		includes  features:
+//   -- creates new customer types
+//   -- hash objects at apporiate location
+//
+// Assumptions:
+//   -- customer types will be stored in the apporiate location
+//   -- invalid types are ignored
+//---------------------------------------------------------------------------
 #ifndef CUSTOMERFACTORY_H
 #define CUSTOMERFACTORY_H
 #include "factory.h"
@@ -5,7 +19,7 @@
 #include "object.h"
 
 class CustomerFactory : public Factory{
-	static const int SIZE = 1;
+	static const int SIZE = 1; //size of factory
 public:
 	CustomerFactory();
 	virtual ~CustomerFactory();
