95044083408e02a2b7fafb25fe844e872d93f689
asmusj <asmusj@users.noreply.github.com>
2014-06-02 17:31:13 -0700

    updated comments

--- Project1/userfactory.h
+++ Project1/userfactory.h
@@ -1,3 +1,17 @@
+//---------------------------------------------------------------------------
+// userFactory.h
+// derived class for factorys, stores user objects
+// Authors: Julio Perez, Jeffrey Asmus
+//---------------------------------------------------------------------------
+// userObject class:  
+//		includes  features:
+//   -- creates new user types
+//   -- hash objects at apporiate location
+//
+// Assumptions:
+//   -- Genre types will be stored in the apporiate location
+//   -- invalid types are ignored
+//---------------------------------------------------------------------------
 #ifndef USERFACTORY_H
 #define USERFACTORY_H
 
