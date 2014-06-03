0124638449b327dd32b1f655343c481bf498ac83
asmusj <asmusj@users.noreply.github.com>
2014-06-02 17:07:19 -0700

    updated comments

--- Project1/factory.cpp
+++ Project1/factory.cpp
@@ -1,3 +1,13 @@
+//---------------------------------------------------------------------------
+// Factory.h
+//  base class that helps stores customers, movies, and actions
+// Authors: Julio Perez, Jeffrey Asmus
+//---------------------------------------------------------------------------
+// factory class:  
+//		includes  features:
+//   -- creates objects
+//
+//---------------------------------------------------------------------------
 #include "factory.h"
 
 Factory::Factory(){}
