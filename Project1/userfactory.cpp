f822b40c3a69b1f96a172a2084ecb99c84e88136
asmusj <asmusj@users.noreply.github.com>
2014-06-02 17:32:55 -0700

    updated comments

--- Project1/userfactory.cpp
+++ Project1/userfactory.cpp
@@ -1,9 +1,29 @@
+//---------------------------------------------------------------------------
+// userFactory.h
+// derived class for factorys, stores user objects
+// Authors: Julio Perez, Jeffrey Asmus
+//---------------------------------------------------------------------------
+// userObject class:  
+//		includes  features:
+//   -- creates new user types
+//   -- hash objects at apporiate location
+//
+// Assumptions:
+//   -- Genre types will be stored in the apporiate location
+//   -- invalid types are ignored
+//---------------------------------------------------------------------------
 #include "userfactory.h"
 
+//---------------------------------------------------------------------------
+//constructor
+//create new customer obj 
 UserFactory::UserFactory() :Factory(){
 	objFactory[0] = new CustomerObj();
 }
 
+//---------------------------------------------------------------------------
+//destructor
+//deallocate memory from objfactory
 UserFactory::~UserFactory(){
 	for (int i = 0; i < SIZE; i++){
 		if (objFactory[i] != NULL){
@@ -13,11 +33,17 @@ UserFactory::~UserFactory(){
 	}
 }
 
+//---------------------------------------------------------------------------
+//createIt
+//create new user obj at subscipt index
 Object* UserFactory::createIt(char userCode){
 	int subscript = hash(userCode);
 	return objFactory[subscript]->create();
 }
 
+//---------------------------------------------------------------------------
+//hash
+//determine the index to store user obj
 int UserFactory::hash(char userCode){
 		return 0; 
 }
